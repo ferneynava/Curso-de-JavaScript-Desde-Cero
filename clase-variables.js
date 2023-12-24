4d138e948584e0d3ff2b0c04eb88e8742eef2747
ferneynava <ferneynava@gmail.com>
2023-12-24 17:25:57 -0500

    :white_check_mark: [Add] clase variables
    [Add] clase variables

---
+++ clase-variables.js
@@ -0,0 +1,23 @@
+let cajaDeTexto = "Hola";
+console.log(cajaDeTexto);
+
+// Lo NO permitiodo 
+let c = "Woody";
+let cd = "Woody";
+let pcF = "Woody";
+
+
+// Lo permitido 
+let primerNumeroDeLaSerieFibonacci = 0;
+let urlDeLaPagina = "https://www.google.com";
+let idDelUsuario = 123456789;
+
+//let: Let se utiliza para declarar variables que pueden ser reasignadas.
+//Ejemplo
+let contador;
+contador = contador + 1;
+
+//const: const se utiliza para declarar variables que no pueden ser reasignadas.
+//Ejemplo
+const PI = 3.1416;
+
