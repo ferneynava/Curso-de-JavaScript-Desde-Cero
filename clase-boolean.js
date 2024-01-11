07f09b0ca8935a2269515372cf6e9b2d1732bb38
ferneynava <ferneynava@gmail.com>
2024-01-11 14:38:28 -0500

    :white_check_mark: [Add] clase-boolean
    [Add] clase-boolean

---
+++ clase-boolean.js
@@ -0,0 +1,16 @@
+const isActive = true; // is (es)
+const hasPermisson = false; // has (tiene)
+
+// Conversión implícita 
+const resultado = 5 > 3; 
+console.log(resultado);
+
+const name = 'Platzi';
+console.log(!!name); // !! compronbar si una variable tiene un valor o no
+
+// Conversión explícita
+const value = 0;
+const otherValue = -24;
+const explicitBoolean = Boolean(otherValue);
+console.log(explicitBoolean);
+
